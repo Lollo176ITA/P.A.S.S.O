a033b14e4bb4df345a1a99fa7b35a7a98a036f01
Lollo176ITA <116870391+Lollo176ITA@users.noreply.github.com>
2025-11-19 14:59:39 +0100

    feat: aggiungi supporto per Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ScrollToTopButton from "@/components/ScrollToTopButton";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,6 +40,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
         <ScrollToTopButton />
       </body>
     </html>
